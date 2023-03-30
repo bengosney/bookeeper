b13a7a9891be4c4ecfd69032962802da33756ea0
Ben Gosney <bengosney@googlemail.com>
2023-03-30 14:07:17 +0100

    Input helper

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,13 +1,28 @@
 import { QrScanner } from "@yudiel/react-qr-scanner";
-import { useReducer, useState } from "react";
+import { HTMLInputTypeAttribute, HTMLProps, InputHTMLAttributes, ReactNode, useReducer, useState } from "react";
 import QRCode from "react-qr-code";
 import { CouchdbSettings, defaultSettings } from "./hooks/pouchSync";
 import { useLocalStorage } from "./hooks/settings";
+
+import "./Settings.scss";
 
 interface SettingAction {
   field: keyof CouchdbSettings;
   value: string;
 }
+
+interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
+  children: ReactNode;
+}
+
+const Input = ({ children, name, ...props }: InputProps) => {
+  return (
+    <div>
+      <label htmlFor={name}>{children}</label>
+      <input id={name} name={name} {...props} />
+    </div>
+  );
+};
 
 const Settings = () => {
   const [settings, setSettings] = useLocalStorage<CouchdbSettings>("couchdb", defaultSettings);
@@ -41,41 +56,38 @@ const Settings = () => {
         ) : null}
         <button onClick={() => toggleQR()}>{showQR ? "Hide" : "Show"} QR Code</button>
       </div>
-      <div>
-        <label htmlFor="couchdb_database">Sync Database</label>
-        <input
-          name="couchdb_database"
-          value={state.database}
-          onChange={(e) => dispatch({ field: "database", value: e.target.value })}
-        />
-      </div>
-      <div>
-        <label htmlFor="couchdb_server">Sync Server</label>
-        <input
-          name="couchdb_server"
-          value={state.server}
-          onChange={(e) => dispatch({ field: "server", value: e.target.value })}
-        />
-      </div>
-      <div>
-        <label htmlFor="couchdb_username">Username</label>
-        <input
-          name="couchdb_username"
-          autoComplete="off"
-          value={state.username}
-          onChange={(e) => dispatch({ field: "username", value: e.target.value })}
-        />
-      </div>
-      <div>
-        <label htmlFor="couchdb_password">Password</label>
-        <input
-          type="password"
-          name="couchdb_password"
-          autoComplete="off"
-          value={state.password}
-          onChange={(e) => dispatch({ field: "password", value: e.target.value })}
-        />
-      </div>
+      <Input
+        name="couchdb_database"
+        value={state.database}
+        onChange={(e) => dispatch({ field: "database", value: e.target.value })}
+      >
+        Sync Database
+      </Input>
+
+      <Input
+        name="couchdb_server"
+        value={state.server}
+        onChange={(e) => dispatch({ field: "server", value: e.target.value })}
+      >
+        Sync Server
+      </Input>
+      <Input
+        name="couchdb_username"
+        autoComplete="off"
+        value={state.username}
+        onChange={(e) => dispatch({ field: "username", value: e.target.value })}
+      >
+        Username
+      </Input>
+      <Input
+        type="password"
+        name="couchdb_password"
+        autoComplete="off"
+        value={state.password}
+        onChange={(e) => dispatch({ field: "password", value: e.target.value })}
+      >
+        Password
+      </Input>
       <div>
         <button onClick={() => save()}>Save</button>
       </div>
